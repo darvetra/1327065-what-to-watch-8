472ecb238fe89f861e1991cf3238fdad59f4d5b3
Darvetra <darvetra13@gmail.com>
2021-10-24 17:11:22 +0300

    добавляет стейт для карточки фильма в списке

--- project/src/components/movie-card/movie-card.tsx
+++ project/src/components/movie-card/movie-card.tsx
@@ -1,8 +1,26 @@
 import {MovieType} from '../../types/movie';
+import {useState, useEffect} from 'react';
 
 function MovieCard(props: {movie: MovieType}): JSX.Element {
+
+  const [movieCardHover, setMovieCardHover] = useState(false);
+
+  useEffect(() => {
+    if(movieCardHover) {
+
+      // До момента реализации воспроизведения видео по наведению на карточку фильма, иначе просто movieCardHover нигде не используется, что странно
+
+      /* eslint-disable no-alert, no-console */
+      console.log('работает');
+    }
+  });
+
   return (
-    <article className="small-film-card catalog__films-card">
+    <article
+      className="small-film-card catalog__films-card"
+      onMouseEnter={() => setMovieCardHover(true)}
+      onMouseLeave={() => setMovieCardHover(false)}
+    >
       <div className="small-film-card__image">
         <img src={props.movie.previewImage} alt={props.movie.name} width="280" height="175"/>
       </div>
