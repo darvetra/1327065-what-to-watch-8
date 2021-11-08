220db72b0ee7e79726b5dc41ab196c9a53c91468
Darvetra <darvetra13@gmail.com>
2021-11-08 16:59:42 +0300

    подключает Redux к проекту

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app';
+import {reducer} from './store/reducer';
 
 import {promoMovie} from './mocks/promo';
 import {moviesList} from './mocks/films';
@@ -9,8 +13,12 @@ import {commentsList} from './mocks/comments';
 // моковые данные для превью плеера и добавления ревью
 const movie = promoMovie;
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App promoMovie={promoMovie} movies={moviesList} movie={movie} comments={commentsList}/>
+    <Provider store = {store}>
+      <App promoMovie={promoMovie} movies={moviesList} movie={movie} comments={commentsList}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
